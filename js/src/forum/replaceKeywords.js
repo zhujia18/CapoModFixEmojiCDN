af11106b23a08e89fd93b6fc3a52008302df0da9
Jasper <>
2022-11-27 19:45:03 +0800

    update

--- js/src/forum/replaceKeywords.js
+++ js/src/forum/replaceKeywords.js
@@ -16,10 +16,11 @@ export default function () {
   const post = this.attrs.post;
 
   const mappings = JSON.parse('{ "https://cdn.jsdelivr.net":"https://testingcf.jsdelivr.net" }');
-
+  console.log('aaaaa');
   if (mappings === {}) return;
+  console.log('bbbbbb');
   if (Object.keys(mappings).length === 0) return;
-
+  console.log('cccccc');
   // filtro per evitare il parsing doppio delle parole
   Object.keys(mappings).filter((w) => {
     let regex = new RegExp('\\b(' + w + ')\\b(?![^<]*>|[^<>]*</[^p])', 'gi');
@@ -27,9 +28,11 @@ export default function () {
     this.attrs.post.data.attributes.contentHtml = post.contentHtml().replace(regex, (match) => {
       let tooltip = mappings[match.toLowerCase()];
       if (tooltip) {
+        console.log('dddddd');
         return '11111'
         return `<span class="definition" data-tooltip="${tooltip}">${match}</span>`;
       } else {
+        console.log('eeeeee');
         return '22222'
         return match;
       }
