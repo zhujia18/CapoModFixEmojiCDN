8cb7f6d9ef3aa66a7e7c8f0ab0c24d4b8fca98d1
Jasper <>
2022-11-27 18:32:58 +0800

    update

--- extend.php
+++ extend.php
@@ -5,6 +5,7 @@ namespace FoF\SecureHttps;
 use Flarum\Extend;
 
 return [
-    (new Extend\Formatter())
-        ->render(FormatImages::class)
+    (new Extend\Formatter())->render(FormatImages::class),
+
+    (new Extend\ApiSerializer(BasicPostSerializer::class))->attributes(ModifyContentHtml::class),
 ];
